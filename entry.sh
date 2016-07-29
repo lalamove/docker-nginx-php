e741152e1cc774bbd5af354f0ebb6ff299981e99
Simon Tse <system@lalamove.com>
2016-07-29 11:40:15 +0800

    use colon separated paths

--- entry.sh
+++ entry.sh
@@ -3,15 +3,14 @@
 user=1000
 group=1000
 
-if [ x"$WRITABLE_PATH" != x ]
+if [ x"$WRITABLE_PATHS" != x ]
 then
 
-writablePaths=$(echo "$WRITABLE_PATHS" | sed 's/,/ /g')
-for path in $writablePaths
+IFS=":"
+for path in $WRITABLE_PATHS
 do
 	chown -R $user:$group "$path"
 done
-
 fi
 
 /usr/bin/python /usr/bin/supervisord -c /opt/docker/etc/supervisor.conf --logfile /dev/null --pidfile /dev/null --user root
